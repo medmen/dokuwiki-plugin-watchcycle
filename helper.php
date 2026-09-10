1a864ace198c76185b41d4b888feb68c7341d545
med-men <med-men@gmx.net>
2026-09-10 22:00:54 +0200

    added logging (debug mode) when email gets not sent due to rate limit

--- helper.php
+++ helper.php
@@ -211,7 +211,7 @@ class helper_plugin_watchcycle extends Plugin
                 $lastMail = $dbHelper->getLastMail($page);
                 if ($lastMail > 0 && $this->daysAgo($lastMail) < $mail_limit) {
                     // write log message to explain and test
-                    $details = "will NOT Send a watchcycle reminder email for $page , because a rate limit of $mail_limit days is in effect and last reminder was send $lastMail days ago.";
+                    $details = "will NOT Send a watchcycle reminder email for $page , because a rate limit of $mail_limit days is in effect and last reminder was sent ". $this->daysAgo($lastMail)." days ago.";
                     Logger::getInstance('plugin_watchcycle')->debug('Not sending email (rate limit)', $details, __FILE__, __LINE__);
                     
                     return false;
